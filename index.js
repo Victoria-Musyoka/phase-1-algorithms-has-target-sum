5c09ebd3d5841647f24337080b63aea5aaad7ff2
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-18 15:05:05 +0300

    completing assignment

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+    // Write your algorithm here
+      const seeNumbers = {}; 
+      for(const item of array){
+        const complement=target-item;
+        if (seeNumbers[complement]) return true;
+        seeNumbers[item]=true
+      }
+      return false;
+    }
+  
 
 /* 
   Write the Big O time complexity of your function here
@@ -9,10 +18,16 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
+// hasTargetSum([3,8,12,4,11,7],10)
+// hasTargetSum([22, 19, 4, 6, 30], 25);
+// hasTargetSum([1, 2, 5], 4);
+
+// /*/
 
 /*
   Add written explanation of your solution here
 */
+// if the target is inside the array, then it is false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
